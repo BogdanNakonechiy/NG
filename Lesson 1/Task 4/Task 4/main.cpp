c986833b10327390497f7388bf7b886217537047
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-05-17 00:23:34 +0300

    Task 4 was done! (again)

--- Lesson 1/Task 4/Task 4/main.cpp
+++ Lesson 1/Task 4/Task 4/main.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 int main()
 {
-    int prise = 0;
-    float a = 0, cost = 0;
+    float cost = 0, prise = 0;
     cout << "Motherboard price: ";
-    cin >> a;
-    cost = a;
+    cin >> prise;
+    cost = prise;
     cout << "Videocard price: ";
-    cin >> a;
-    cost = cost + a;
+    cin >> prise;
+    cost += prise;
     cout << "CPU price: ";
-    cin >> a;
-    cost = cost + a;
+    cin >> prise;
+    cost += prise;
     cout << "Discount: ";
-    cin >> a;
-    cost -= cost * (a / 100);
+    cin >> prise;
+    cost -= cost * (prise / 100);
     cout << "Your PC will cost - " << cost;
     return 0;
 }
