c0a9633bc720cacf4cf48226a718854ea809b8fd
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-05-17 00:34:43 +0300

    Task 3 was done!

---
+++ Lesson 2/Task 3/Task 3/Task 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Numb, HashNumb = 0;
+	cout << "Enter your number: ";
+	cin >> Numb;
+	for (int i = Numb; i > 0; i /= 10)
+	{
+		if (i % 10 == (i / 10) % 10 || i == Numb % 10)
+			HashNumb += i % 10;
+	}
+	cout << "Your hash-code is " << HashNumb << endl;
+}
