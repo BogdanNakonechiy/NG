b10144010237d0448107fbe2cfd857aab2146726
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-03 01:08:58 +0300

    roots are here

---
+++ Lesson 1/Task 5/Task 5/Task 5.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, D;
+    cout << "Enter A: ";
+    cin >> a;
+    cout << "Enter B: ";
+    cin >> b;
+    cout << "Enter C: ";
+    cin >> c;
+    D = pow(b, 2) - 4 * a * c;
+    if (D < 0)
+    {
+        cout << endl << "No roots found." << endl;
+    }
+    if (D == 0)
+    {
+        cout << endl << "D = 0" << endl
+                     << "Root = " << -b / (2 * a) << endl;
+    }
+    if(D > 0)
+    {
+        cout << endl <<  "D > 0"  << endl 
+                     << "Root 1:" << -b - sqrt(D) / (2 * a) << endl
+                     << "Root 2:" << -b + sqrt(D) / (2 * a) << endl;
+    }
+    
+}
