8c5f47d06ea750d8e674ac41ce0373005acaef0d
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-03 18:39:56 +0300

    str comparison was done!

---
+++ Lesson 4/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char firstStr[100];
+    char secondStr[100];
+    
+    cout << "Enter your first string: ";
+    cin >> firstStr;
+    cout << endl << "Enter your second string: ";
+    cin >> secondStr;
+    
+    cout << endl;
+
+    if (strlen(firstStr) > strlen(secondStr))
+        cout << "First ";
+    else
+        cout << "Second ";
+    cout << "string is bigger!" << endl;
+}
