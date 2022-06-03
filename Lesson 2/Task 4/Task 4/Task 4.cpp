c3d11e3716b5d14f5c160ccf1c740ccabe232b4b
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-03 12:25:42 +0300

    work in progress

---
+++ Lesson 2/Task 4/Task 4/Task 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size/2 - i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 0; k < i*2-1; k++)
+        {
+            cout << "0";
+        }
+        cout << endl;
+    }
+}
