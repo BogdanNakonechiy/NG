70cf160ba9cf66878794a56bcdc20a5e27072608
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-03 12:35:19 +0300

    search numb was done

---
+++ Lesson 3/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int SizeArray, SearchNumber;
+    
+    cout << "Enter size of array: ";
+    cin >> SizeArray;
+
+    int *Array = new int[SizeArray];
+    
+    
+    for (int i = 0; i < SizeArray; i++)
+    {
+        cout << "Enter element num " << i + 1<< ": ";
+        cin >> Array[i];
+    }
+    
+    cout << "What number you want to check:";
+    cin >> SearchNumber;
+    cout << endl;
+    for (int i = 0; i < SizeArray; i++)
+    {
+        if (Array[i] == SearchNumber)
+        {
+            cout << "I know this number!" << endl;
+            break;
+        }
+        if (i + 1 == SizeArray)
+            cout << "I don't know this number." << endl;
+    }  
+}
