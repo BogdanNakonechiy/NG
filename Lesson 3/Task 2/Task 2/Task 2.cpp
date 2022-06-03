2fc8a8e9e6d8c475c5b04e47e22a34e70e16de33
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-03 13:00:28 +0300

    array comparison

--- Lesson 3/Task 2/Task 2/Task 2.cpp
+++ Lesson 3/Task 2/Task 2/Task 2.cpp
@@ -9,11 +9,13 @@ int main()
 {
     srand(time(0));
     int FirstArray[4], SecondArray[4], FirstNumb = 0, SecondNumb = 0;
+    
     for (int i = 0; i < 4; i++)
     {
         FirstArray[i] = rand() % 9;
         SecondArray[i] = rand() % 9;
     }
+    
     for (int i = 0; i < 4; i++)
     {
         FirstNumb += FirstArray[i] * pow(10, 3 - i);
