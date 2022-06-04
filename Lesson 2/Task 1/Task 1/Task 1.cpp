5636746d91594989bbdfbf051349c5a5de739634
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-04 13:35:21 +0300

    code style fixed

---
+++ Lesson 2/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size;
+	cout << "Enter size: ";
+	cin >> size;
+	for (int i = 1; i <= size; i++)
+	{
+		for (int j = 1; j <= size; j++)
+		{
+			if (i > 1 && i < size && j > 1 && j < size)
+				cout << " ";
+			else
+				cout << "*";
+		}
+		cout << endl;
+	}
+}
