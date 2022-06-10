5d4267bce18dd81bebda66bedecd2592c4654888
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-10 18:58:35 +0300

    array comparison was done!

---
+++ Lesson 4/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char firstStr[100];
+    char secondStr[100];
+    
+    cout << "Enter your first string: ";
+    cin >> firstStr;
+    cout << endl << "Enter your second string: ";
+    cin >> secondStr;
+    
+    cout << endl;
+
+    for (int i = 0; i < sizeof(firstStr) / sizeof(firstStr[0]); i++)
+    {
+        if (firstStr[i] == NULL)
+        {
+            cout << "Second ";
+            break;
+        }
+        
+        if (secondStr[i] == NULL)
+        {
+            cout << "First ";
+            break;
+        }
+    }
+    
+    cout << "string is bigger!" << endl;
+}
