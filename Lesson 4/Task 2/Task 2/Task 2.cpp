d73bb75046b84715196bc0740f873b57d98f1497
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-10 19:23:13 +0300

    task was done!

---
+++ Lesson 4/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[100];
+    cout << "Enter str: ";
+    cin >> str;
+    
+    for (int i = 0; i < sizeof(str) / sizeof(str[0]); i++)
+    {
+        if (i % 2 == 0 && str[i] >= 'A' && str[i] <= 'Z')
+            cout << str[i] << endl;
+    }
+
+}
