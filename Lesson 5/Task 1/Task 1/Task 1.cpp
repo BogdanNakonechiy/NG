71e29fad1183236ac911007531dbab2a792bd129
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-06-29 19:53:01 +0300

    functions for cubes is here

---
+++ Lesson 5/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int cubeX(int X, int Y, int size)
+{
+    if (X >= size || Y >= size || X <= 1 || Y <= 1)
+        cout << "*";
+    else
+        cout << " ";
+    if (X == 1)
+        return 0;
+    X--;
+    cubeX(X, Y, size);
+}
+
+int cubeY(int Y, int X, int size)
+{
+    cubeX(X, Y, size);
+    cout << endl;
+    Y--;
+    if (Y == 0)
+        return 0;
+    cubeY(Y, X, size);
+}
+
+int main()
+{
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    cubeY(size, size, size);
+
+}
