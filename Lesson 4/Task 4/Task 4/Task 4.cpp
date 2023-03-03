b8bad8d33b2b6dfbc5f25665ab90641351ad2965
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-03-03 23:04:08 +0200

    test

---
+++ Lesson 4/Task 4/Task 4/Task 4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Word Amount
+
+int main()
+{
+    const int size = 100;
+    
+    char string[size];
+    char test[size];
+    
+    int counter = 0;
+
+    cout << "Enter the setence: ";
+    cin >> string;
+    
+    cout << endl;
+
+    cout << "Enter the setence: ";
+    cin >> test;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (string[i] == NULL || test[counter] == NULL)
+            break;
+        
+        if (string[i] >= 'A' && string[i] <= 'z')
+        {
+            if (string[i] == test[counter] || abs(string[i] - test[counter]) == 32)
+            {
+                counter++;
+                if (counter == 4)
+                    cout << "aoaoa";
+            }
+            else
+                counter = 0;
+        }
+        
+    }
+}
---
+++ Lesson 5/Task 4/Task 4/Task 4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// discriminant
+
+int input(int a = 0, int b = 0, int c = 0)
+{
+    cout << "Enter A: ";
+    cin >> a;
+    cout << "Enter B: ";
+    cin >> b;
+    cout << "Enter C: ";
+    cin >> c;
+
+    return a, b, c;
+}
+
+int result(int a, int b, int D)
+{
+    if (D < 0)
+    {
+        cout << endl << "No roots found." << endl;
+    }
+    if (D == 0)
+    {
+        cout << endl << "D = 0" << endl
+             << "Root = " << -b / (2 * a) << endl;
+    }
+    if (D > 0)
+    {
+        cout << endl << "D > 0" << endl
+             << "Root 1:" << -b - sqrt(D) / (2 * a) << endl
+             << "Root 2:" << -b + sqrt(D) / (2 * a) << endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    cout << endl << discriminant(a, b, c, D) << endl;
+    D = pow(b, 2) - 4 * a * c;
+}
